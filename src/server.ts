887ee10cac12b5f3f2a92ad32a7524a5275052bc
Ernane Ferreira <Ernane.junior25@gmail.com>
2022-02-17 18:31:24 -0300

    Implementando politica de corst

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,23 @@
 import "reflect-metadata";
 import 'express-async-errors';
 import express from "express";
+import cors from "cors";
 import './database';
 import { routes } from "./routes";
 
 const app = express();
 
+const allowedOrigins = ['http://localhost:5000', 'http://localhost:3000'];
+app.use(cors({
+  origin: (origin, callback)  => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 app.use(express.json());
 app.use(routes);
 
